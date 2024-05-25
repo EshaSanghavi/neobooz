a2821c6c4a64247319359583da6ba635d6a941be
EshaSanghavi <eshacsanghavi@gmail.com>
2024-05-25 15:49:09 +0530

    blog

--- resources/views/admin-views/blog/list.blade.php
+++ resources/views/admin-views/blog/list.blade.php
@@ -13,48 +13,6 @@
             </h2>
         </div>
 
-       
-        <div class="row mt-20">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="px-3 py-4">
-                        <div class="row align-items-center">
-                            <div class="col-lg-4">
-
-                                <form action="{{ url()->current() }}" method="GET">
-                                    <div class="input-group input-group-custom input-group-merge">
-                                        <div class="input-group-prepend">
-                                            <div class="input-group-text">
-                                                <i class="tio-search"></i>
-                                            </div>
-                                        </div>
-                                        <input id="datatableSearch_" type="search" name="searchValue"
-                                               class="form-control"
-                                               placeholder="{{ translate('search_Product_Name') }}"
-                                               aria-label="Search orders"
-                                               value="{{ request('searchValue') }}">
-                                        <input type="hidden" value="{{ request('status') }}" name="status">
-                                        <button type="submit" class="btn btn--primary">{{ translate('search') }}</button>
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="col-lg-8 mt-3 mt-lg-0 d-flex flex-wrap gap-3 justify-content-lg-end">
-
-                                
-                                <a href="{{ route('admin.blog.add') }}" class="btn btn--primary">
-                                    <i class="tio-add"></i>
-                                    <span class="text">{{ translate('add_new_blog') }}</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    
-
-                   
-                </div>
-            </div>
-        </div>
     </div>
 
     <span id="message-select-word" data-text="{{ translate('select') }}"></span>
