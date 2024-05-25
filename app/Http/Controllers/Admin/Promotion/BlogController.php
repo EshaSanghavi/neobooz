104727d863e7102eaf83ccaf09339203034a4667
EshaSanghavi <eshacsanghavi@gmail.com>
2024-05-25 14:36:36 +0530

    blog

--- app/Http/Controllers/Admin/Promotion/BlogController.php
+++ app/Http/Controllers/Admin/Promotion/BlogController.php
@@ -14,20 +14,81 @@ use Illuminate\Support\Facades\Http;
 use Image;
 use File;
 use Auth;
+
+use App\Contracts\Repositories\AttributeRepositoryInterface;
+use App\Contracts\Repositories\BannerRepositoryInterface;
+use App\Contracts\Repositories\BrandRepositoryInterface;
+use App\Contracts\Repositories\CartRepositoryInterface;
+use App\Contracts\Repositories\CategoryRepositoryInterface;
+use App\Contracts\Repositories\ColorRepositoryInterface;
+use App\Contracts\Repositories\DealOfTheDayRepositoryInterface;
+use App\Contracts\Repositories\FlashDealProductRepositoryInterface;
+use App\Contracts\Repositories\ProductRepositoryInterface;
+use App\Contracts\Repositories\ReviewRepositoryInterface;
+use App\Contracts\Repositories\TranslationRepositoryInterface;
+use App\Contracts\Repositories\VendorRepositoryInterface;
+use App\Contracts\Repositories\WishlistRepositoryInterface;
+use App\Enums\ViewPaths\Admin\Product;
+use App\Enums\WebConfigKey;
+use App\Events\ProductRequestStatusUpdateEvent;
+use App\Exports\ProductListExport;
+use App\Http\Controllers\BaseController;
+use App\Http\Requests\Admin\ProductDenyRequest;
+use App\Http\Requests\ProductAddRequest;
+use App\Http\Requests\ProductUpdateRequest;
+use App\Services\ProductService;
+use App\Traits\FileManagerTrait;
+use Brian2694\Toastr\Facades\Toastr;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Maatwebsite\Excel\Facades\Excel;
+use mysql_xdevapi\Exception;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+    
 class BlogController extends Controller
 {
-    public function __construct()
-    {
-        
+    use FileManagerTrait {
+        delete as deleteFile;
+        update as updateFile;
     }
 
-    public function index()
+    public function __construct(
+        private readonly CategoryRepositoryInterface         $categoryRepo,
+        private readonly BrandRepositoryInterface            $brandRepo,
+        private readonly ProductRepositoryInterface          $productRepo,
+        private readonly VendorRepositoryInterface           $sellerRepo,
+        private readonly ColorRepositoryInterface            $colorRepo,
+        private readonly AttributeRepositoryInterface        $attributeRepo,
+        private readonly TranslationRepositoryInterface      $translationRepo,
+        private readonly CartRepositoryInterface             $cartRepo,
+        private readonly WishlistRepositoryInterface         $wishlistRepo,
+        private readonly FlashDealProductRepositoryInterface $flashDealProductRepo,
+        private readonly DealOfTheDayRepositoryInterface     $dealOfTheDayRepo,
+        private readonly ReviewRepositoryInterface           $reviewRepo,
+        private readonly BannerRepositoryInterface           $bannerRepo,
+    )
     {
-        $blogs = Blog::with('category','comments')->get();
-        $frontend_url = $setting->frontend_url;
-        $frontend_url = $frontend_url.'/blogs/blog?slug=';
+    }
 
-        return view('admin-views.blog.list', compact('blogs','frontend_url'));
+    public function index(Request|null $request, string $type = null): View
+    {
+        return $this->getListView(request: $request, type: $type);
+    }
+
+    public function getAddView(): View
+    {
+        $categories = $this->categoryRepo->getListWhere(filters: ['position' => 0], dataLimit: 'all');
+        $brands = $this->brandRepo->getListWhere(dataLimit: 'all');
+        $brandSetting = getWebConfig(name: 'product_brand');
+        $digitalProductSetting = getWebConfig(name: 'digital_product');
+        $colors = $this->colorRepo->getList(orderBy: ['name' => 'desc'], dataLimit: 'all');
+        $attributes = $this->attributeRepo->getList(orderBy: ['name' => 'desc'], dataLimit: 'all');
+        $languages = getWebConfig(name: 'pnc_language') ?? null;
+        $defaultLanguage = $languages[0];
+
+        return view(Product::ADD[VIEW], compact('categories', 'brands', 'brandSetting', 'digitalProductSetting', 'colors', 'attributes', 'languages', 'defaultLanguage'));
     }
 
 
