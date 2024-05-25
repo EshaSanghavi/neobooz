0349369b87ff9bd3c2c431398295de30b11e6389
EshaSanghavi <eshacsanghavi@gmail.com>
2024-05-25 12:29:38 +0530

    app

---
+++ app/Models/Blog.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Facades\App;
+
+class Blog extends Model
+{
+    
+}
