5b53b7a21ce7240be3a55bf0fdd8576d98d12668
EshaSanghavi <eshacsanghavi@gmail.com>
2024-05-28 15:22:25 +0530

    home blogs

--- resources/themes/default/web-views/partials/_blog-home-section.blade.php
+++ resources/themes/default/web-views/partials/_blog-home-section.blade.php
@@ -4,12 +4,12 @@
             <div class="d-flex __gap-6px flex-between px-sm-3">
                 <div class="category-product-view-title">
                 <span class="for-feature-title font-bold __text-20px text-uppercase">
-                        {{$category['name']}}
+                        {{$blog['name']}}
                 </span>
                 </div>
                 <div class="category-product-view-all">
                     <a class="text-capitalize view-all-text text-nowrap web-text-primary"
-                       href="{{route('products',['id'=> $category['id'],'data_from'=>'category','page'=>1])}}">
+                       href="{{route('blogs',['id'=> $blog['id'],'page'=>1])}}">
                         {{ translate('view_all')}}
                         <i class="czi-arrow-{{Session::get('direction') === "rtl" ? 'left mr-1 ml-n1 mt-1 float-left' : 'right ml-1 mr-n1'}}"></i>
                     </a>
@@ -19,17 +19,17 @@
             <div class="mt-2">
                 <div class="carousel-wrap-2 d-none d-sm-block">
                     <div class="owl-carousel owl-theme category-wise-product-slider">
-                        @foreach($category['products'] as $key => $product)
-                            @include('web-views.partials._category-single-product',['product'=>$product,'decimal_point_settings'=>$decimal_point_settings])
+                        @foreach($blog as $blog)
+                            @include('web-views.partials._single-blog',['blog'=>$blog,'decimal_point_settings'=>$decimal_point_settings])
                         @endforeach
                     </div>
                 </div>
                 <div class="d-sm-none">
                     <div class="row g-2">
-                        @foreach($category['products'] as $key=>$product)
+                        @foreach($blog as $blog)
                             @if($key < 4)
                                 <div class="col-6">
-                                    @include('web-views.partials._category-single-product',['product'=>$product,'decimal_point_settings'=>$decimal_point_settings])
+                                    @include('web-views.partials._single-blog',['blog'=>$blog,'decimal_point_settings'=>$decimal_point_settings])
                                 </div>
                             @endif
                         @endforeach
