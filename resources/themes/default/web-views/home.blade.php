ef7ab0237e0ac8f40231aa792719c370f549b7df
EshaSanghavi <eshacsanghavi@gmail.com>
2024-05-28 15:31:12 +0530

    blog

--- resources/themes/default/web-views/home.blade.php
+++ resources/themes/default/web-views/home.blade.php
@@ -267,11 +267,36 @@
         @endif
 
 
-        @if ($blogs->count() > 0)
-            @foreach($blogs as $blog)
-                @include('web-views.partials._blog-home-section')
-            @endforeach
-        @endif
+        <section class="container rtl pb-4 px-max-sm-0">
+    <div class="__shadow-2">
+        <div class="__p-20px rounded bg-white overflow-hidden">
+            <div class="d-flex __gap-6px flex-between px-sm-3">
+                <div class="category-product-view-title">
+                <span class="for-feature-title font-bold __text-20px text-uppercase">
+                        {{$blog['name']}}
+                </span>
+                </div>
+                <div class="category-product-view-all">
+                    <a class="text-capitalize view-all-text text-nowrap web-text-primary"
+                       href="{{route('blogs',['id'=> $blog['id'],'page'=>1])}}">
+                        {{ translate('view_all')}}
+                        <i class="czi-arrow-{{Session::get('direction') === "rtl" ? 'left mr-1 ml-n1 mt-1 float-left' : 'right ml-1 mr-n1'}}"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="mt-2">
+                <div class="carousel-wrap-2 d-none d-sm-block">
+                    <div class="owl-carousel owl-theme category-wise-product-slider">
+                        @foreach($blog as $blog)
+                            @include('web-views.partials._single-blog',['blog'=>$blog])
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
         
         
         @php($companyReliability = getWebConfig(name: 'company_reliability'))
