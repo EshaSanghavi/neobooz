942269efc46439132f732efa18ae86d68d245efc
EshaSanghavi <eshacsanghavi@gmail.com>
2024-05-28 17:18:44 +0530

    blogs

--- resources/themes/default/web-views/pages/blogs.blade.php
+++ resources/themes/default/web-views/pages/blogs.blade.php
@@ -1,9 +1,33 @@
 @extends('layouts.front-end.app')
 
 @section('title', translate('all_Blogs'))
- 
-@endsection
 
-@push('script')
-    <script src="{{theme_asset(path: 'public/assets/front-end/vendor/nouislider/distribute/nouislider.min.js')}}"></script>
+@push('css_or_js')
+    <meta property="og:image" content="{{dynamicStorage(path: 'storage/app/public/company')}}/{{$web_config['web_logo']->value}}"/>
+    <meta property="og:title" content="Blogs of {{$web_config['name']->value}} "/>
+    <meta property="og:url" content="{{env('APP_URL')}}">
+    <meta property="og:description" content="{{ substr(strip_tags(str_replace('&nbsp;', ' ', $web_config['about']->value)),0,160) }}">
+    <meta property="twitter:card" content="{{dynamicStorage(path: 'storage/app/public/company')}}/{{$web_config['web_logo']->value}}"/>
+    <meta property="twitter:title" content="Blogs of {{$web_config['name']->value}}"/>
+    <meta property="twitter:url" content="{{env('APP_URL')}}">
+    <meta property="twitter:description" content="{{ substr(strip_tags(str_replace('&nbsp;', ' ', $web_config['about']->value)),0,160) }}">
 @endpush
+
+@section('content')
+
+    <div class="container pb-5 mb-2 mb-md-4 rtl text-align-direction">
+            <div class="bg-primary-light rounded-10 my-4 p-3 p-sm-4"
+             data-bg-img="{{ theme_asset(path: 'public/assets/front-end/img/media/bg.png') }}">
+            <div class="d-flex flex-column gap-1 text-primary">
+                <h4 class="mb-0 text-start fw-bold text-primary text-uppercase">
+                    {{ translate('Blogs') }}
+                </h4>
+            </div>
+        </div>
+
+        <div class="row">
+        
+        </div>
+
+    </div>   
+@endsection
