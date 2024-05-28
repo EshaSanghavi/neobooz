dc3d1813ff246547ce6c913601721599c6490c1b
EshaSanghavi <eshacsanghavi@gmail.com>
2024-05-28 16:39:10 +0530

    blogs

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -164,16 +164,14 @@ class WebController extends Controller
 
     public function all_blogs(Request $request)
     {
-        $brand_status = BusinessSetting::where(['type' => 'product_brand'])->value('value');
-        session()->put('product_brand', $brand_status);
-        if($brand_status == 1){
-            $order_by = $request->order_by ?? 'desc';
-            $brands = Brand::active()->withCount('brandProducts')->orderBy('name', $order_by)
-                                    ->when($request->has('search'), function($query) use($request){
-                                    $query->where('name', 'LIKE', '%' . $request->search . '%');
-                                })->latest()->paginate(15)->appends(['order_by'=>$order_by, 'search'=>$request->search]);
-
-            return view(VIEW_FILE_NAMES['all_brands'], compact('brands'));
+        $blogs = Blog::leftjoin('blog_categories', 'blogs.blog_category_id', '=', 'blog_categories.id')
+            ->where('blogs.status',1)
+            ->orderBy('blogs.id','desc')
+            ->select('blogs.*', 'blog_categories.name as blog_category')
+            ->get();
+            
+        if($blog){
+            return view(VIEW_FILE_NAMES['blogs'], compact('blogs'));
         }else{
             return redirect()->route('home');
         }
