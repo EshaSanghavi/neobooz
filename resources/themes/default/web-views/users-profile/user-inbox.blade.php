e8ea8be0e5ab46e4960707f7cfad410a8068f3fd
EshaSanghavi <eshacsanghavi@gmail.com>
2024-05-30 04:10:26 +0530

    admin

--- resources/themes/default/web-views/users-profile/user-inbox.blade.php
+++ resources/themes/default/web-views/users-profile/user-inbox.blade.php
@@ -203,11 +203,11 @@
 
                                                 @if($last_chat->delivery_man_id)
                                                     <img alt="" class="img" src="{{ getValidImage(path: 'storage/app/public/delivery-man/'.$last_chat->deliveryMan->image, type: 'avatar') }}">
-                                                 @elseif(isset($last_chat->admin_id) && $last_chat->admin_id != 0)
-                                                    <img alt="" class="img" src="{{ getValidImage(path: 'storage/app/public/admin/'.$last_chat->admin->image, type: 'avatar'">
                                                 @else
                                                     @if(isset($last_chat->admin_id) && $last_chat->admin_id == 0)
                                                         <img alt="" class="img" src="{{ getValidImage(path: 'storage/app/public/company/'.($web_config['fav_icon']->value), type: 'shop') }}">
+                                                    @elseif($last_chat->admin_id != 0)
+                                                        <img alt="" class="img" src="{{ getValidImage(path: 'storage/app/public/admin/'.$last_chat->admin->image, type: 'avatar') }}">
                                                     @else
                                                         <img alt="" class="img" src="{{ getValidImage(path: 'storage/app/public/shop/'.($last_chat->shop->image), type: 'shop') }}">
                                                     @endif
