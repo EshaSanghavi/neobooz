3a9dc8e6c21091030ccffcb7476cd6879bfc3c91
EshaSanghavi <eshacsanghavi@gmail.com>
2024-05-29 11:38:20 +0530

    navbar

--- resources/themes/default/layouts/front-end/partials/_header.blade.php
+++ resources/themes/default/layouts/front-end/partials/_header.blade.php
@@ -237,7 +237,7 @@
                                         </a>
                                         @if ($category->childes->count() > 0)
                                             <a data-toggle='dropdown' class='__ml-50px'>
-                                                <i class="czi-arrow-{{Session::get('direction') === "rtl" ? 'left' : 'right'}} __inline-16"></i>
+                                                <i class="czi-arrow-right"></i>
                                             </a>
                                         @endif
 
